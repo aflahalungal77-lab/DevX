dd2c625a88c8b8e15c41267d266f5103d336690c
aflahalungal77-lab <aflahalungal77@gmail.com>
2026-08-16 21:52:14 +0530

    Connect frontend to production API

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected successfully');
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1); // Exit process with failure
+    console.error("MongoDB Connection Error:", error.message);
+    process.exit(1);
   }
-}
+};
+
 module.exports = connectDB;
