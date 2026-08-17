64ebe54c00d22e6b575c070e1fd66ebffc0984b9
aflahalungal77-lab <aflahalungal77@gmail.com>
2026-08-17 17:37:26 +0530

    Add home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,31 +6,47 @@ import Feed from "./pages/feed";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Projects from "./pages/project";
+import { Navigate } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/feed"
-          element={
-            <ProtectedRoute>
-              <Feed />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/projects" element={<Projects />} />{" "}
-      </Routes>
+<Routes>
+
+  <Route path="/" element={<Navigate to="/feed" replace />} />
+
+  <Route path="/login" element={<Login />} />
+
+  <Route path="/register" element={<Register />} />
+
+  <Route
+    path="/feed"
+    element={
+      <ProtectedRoute>
+        <Feed />
+      </ProtectedRoute>
+    }
+  />
+
+  <Route
+    path="/projects"
+    element={
+      <ProtectedRoute>
+        <Projects />
+      </ProtectedRoute>
+    }
+  />
+
+  <Route
+    path="/profile"
+    element={
+      <ProtectedRoute>
+        <Profile />
+      </ProtectedRoute>
+    }
+  />
+
+</Routes>
     </BrowserRouter>
   );
 }
